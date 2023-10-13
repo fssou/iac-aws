44bfc70b688ff54e29d681c940dcef05d075ac05
Franclin Sousa <10904895+fssou@users.noreply.github.com>
2023-10-13 07:28:29 -0300

    feat: add module common iam
    Modulo para criar documento politica para assume role.

---
+++ modules/common_iam/outputs.tf
@@ -0,0 +1,4 @@
+
+output "assume_role_policy" {
+  value = data.aws_iam_policy_document.iac_assume_role
+}
---
+++ modules/common_iam/variables.tf
@@ -0,0 +1,8 @@
+
+variable "gh_repo_id" {
+  type = string
+}
+
+variable "gh_repo_owner" {
+  type = string
+}
