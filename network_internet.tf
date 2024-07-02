a38e4edd8e2c08075a96935479f5c3677900ff2e
Franclin Sousa <10904895+fssou@users.noreply.github.com>
2024-07-01 23:17:00 -0300

    fix: foreach

--- network_internet.tf
+++ network_internet.tf
@@ -72,12 +72,17 @@ resource "aws_route_table" "private_route_table" {
   }
 }
 
-resource "aws_route_table_association" "private_route_table_association" {
-  for_each = toset([
-    aws_subnet.private_a.id,
-    aws_subnet.private_b.id,
-    aws_subnet.private_c.id,
-  ])
-  subnet_id      = each.value
+resource "aws_route_table_association" "private_route_table_association_a" {
+  subnet_id      = aws_subnet.private_a.id
+  route_table_id = aws_route_table.private_route_table.id
+}
+
+resource "aws_route_table_association" "private_route_table_association_b" {
+  subnet_id      = aws_subnet.private_b.id
+  route_table_id = aws_route_table.private_route_table.id
+}
+
+resource "aws_route_table_association" "private_route_table_association_c" {
+  subnet_id      = aws_subnet.private_c.id
   route_table_id = aws_route_table.private_route_table.id
 }
