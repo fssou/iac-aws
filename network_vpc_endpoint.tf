391fd36ccf855be8e4a2517ba042c60170b18372
Franclin Sousa <10904895+fssou@users.noreply.github.com>
2024-07-01 22:29:31 -0300

    fix: aws_vpc_endpoint.s3 to basic form

--- network_vpc_endpoint.tf
+++ network_vpc_endpoint.tf
@@ -3,8 +3,4 @@
 resource "aws_vpc_endpoint" "s3" {
   service_name        = "com.amazonaws.${data.aws_region.current.name}.s3"
   vpc_id              = data.aws_vpc.default.id
-  subnet_ids          = data.aws_subnets.default.ids
-  route_table_ids = [
-    data.aws_route_table.main.id
-  ]
 }
