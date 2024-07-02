885e6f62fa20dc9431cbf7047ce6d446070764ff
Franclin Sousa <10904895+fssou@users.noreply.github.com>
2024-07-02 00:10:10 -0300

    feat: add route_table to vpc endpoint s3

--- network_vpc_endpoint.tf
+++ network_vpc_endpoint.tf
@@ -3,4 +3,8 @@
 resource "aws_vpc_endpoint" "s3" {
   service_name        = "com.amazonaws.${data.aws_region.current.name}.s3"
   vpc_id              = data.aws_vpc.default.id
+  route_table_ids = [
+    aws_route_table.private_route_table.id,
+    data.data.aws_route_table.main.id,
+  ]
 }
