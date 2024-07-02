bcfcc38bf78d56590675657a163fcfdc0bfa262c
Franclin Sousa <10904895+fssou@users.noreply.github.com>
2024-07-01 23:41:49 -0300

    feat: enchement for vpc_endpoint s3

--- network_vpc_endpoint.tf
+++ network_vpc_endpoint.tf
@@ -4,4 +4,18 @@ resource "aws_vpc_endpoint" "s3" {
   service_name        = "com.amazonaws.${data.aws_region.current.name}.s3"
   vpc_id              = data.aws_vpc.default.id
   vpc_endpoint_type   = "Interface"
+  private_dns_enabled = true
+  ip_address_type = "dualstack"
+  dns_options {
+    dns_record_ip_type = "dualstack"
+    private_dns_only_for_inbound_resolver_endpoint = true
+  }
+  security_group_ids = [
+    data.aws_security_group.default.id,
+  ]
+  subnet_ids = concat(
+    [aws_subnet.private_a.id],
+    [aws_subnet.private_b.id],
+    [aws_subnet.private_c.id],
+  )
 }
