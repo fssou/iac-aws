f9a02e9c29dc96e287147e7869efff66ac16ffc2
Franclin Sousa <10904895+fssou@users.noreply.github.com>
2024-07-01 23:32:56 -0300

    fix: aws_vpc_endpoint s3

--- network_vpc_endpoint.tf
+++ network_vpc_endpoint.tf
@@ -4,8 +4,4 @@ resource "aws_vpc_endpoint" "s3" {
   service_name        = "com.amazonaws.${data.aws_region.current.name}.s3"
   vpc_id              = data.aws_vpc.default.id
   vpc_endpoint_type   = "Interface"
-  private_dns_enabled = true
-  security_group_ids = [
-    data.aws_security_group.default.id,
-  ]
 }
